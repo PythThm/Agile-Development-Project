97969ac8d454380a5c72e43cd1365a8752bf4f4c
Roger Hsieh <mhsieh32@my.bcit.ca>
2024-05-06 18:28:23 -0700

    Modify base.html, home.html, style.css, app.py to supply the website  with a homepage

--- app.py
+++ app.py
@@ -14,9 +14,21 @@ app.register_blueprint(api_products_bp, url_prefix="/api/products")
 from routes.api_orders import api_orders_bp
 app.register_blueprint(api_orders_bp, url_prefix="/api/orders")
 
-from routes.html import html_bp
-app.register_blueprint(html_bp, url_prefix="/")
+# from routes.html import html_bp
+# app.register_blueprint(html_bp, url_prefix="/")
 
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/help")
+def help():
+    #return render_template("help.html")
+    return "Haven't built a support page yet xd"
+
+@app.route("/login")
+def login():
+    return "No login yet xd"
 
 app.instance_path = Path("data").resolve()
 app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///{ app.instance_path }/store.sqlite'
